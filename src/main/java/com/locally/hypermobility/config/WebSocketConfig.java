c1e1161cab7e92101d6614431f9511d2b066d960
rihazzahir <rihazzahir@gmail.com>
2023-04-20 13:40:03 +0530

    driver ui initialization, sessionId stored on connection

--- src/main/java/com/locally/hypermobility/config/WebSocketConfig.java
+++ src/main/java/com/locally/hypermobility/config/WebSocketConfig.java
@@ -1,18 +1,19 @@
 package com.locally.hypermobility.config;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationEventPublisher;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-
-import java.util.Map;
 
 
 @Configuration
@@ -27,22 +28,33 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/rider")
-                .addInterceptors(new HandshakeInterceptor() {
-                    @Override
-                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-                        if (request instanceof ServletServerHttpRequest) {
-                            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
-                            HttpSession session = servletRequest.getServletRequest().getSession();
-                            attributes.put("sessionId", session.getId());
-                        }
-                        return true;
-                    }
-
-                    @Override
-                    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
-
-                    }
-                })
+//                .addInterceptors(new HandshakeInterceptor() {
+//                    @Override
+//                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+//                        if (request instanceof ServletServerHttpRequest) {
+//                            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+//                            HttpSession session = servletRequest.getServletRequest().getSession();
+//                            String user  = servletRequest.getPrincipal().getName();
+//                            attributes.put("sessionId", session.getId());
+//                            attributes.put("userId", user);
+//                        }
+//                        return true;
+//                    }
+//
+//                    @Override
+//                    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+//
+//                    }
+//                })
                 .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        AuthorizationManager<Message<?>> myAuthorizationRules = AuthenticatedAuthorizationManager.authenticated();
+        AuthorizationChannelInterceptor authz = new AuthorizationChannelInterceptor(myAuthorizationRules);
+        AuthorizationEventPublisher publisher = new SpringAuthorizationEventPublisher(new GenericApplicationContext());
+        authz.setAuthorizationEventPublisher(publisher);
+        registration.interceptors(new SecurityContextChannelInterceptor(), authz);
     }
 }
